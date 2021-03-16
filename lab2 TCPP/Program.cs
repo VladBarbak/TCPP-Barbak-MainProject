67ab48d3e237c626c3bdfc9bbe5838ee679e56c9
VladBarbak <vladsuper012@gmail.com>
2021-03-16 23:01:14 +0200

    Зміна 1

--- lab2 TCPP/Program.cs
+++ lab2 TCPP/Program.cs
@@ -7,6 +7,7 @@ namespace lab2_TCPP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
