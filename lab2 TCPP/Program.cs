0c2f68b03646f8467012f6c0416d9f6ddb914c15
VladBarbak <60926898+VladBarbak@users.noreply.github.com>
2021-04-08 10:27:56 +0300

    Змінено з браузера

--- lab2 TCPP/Program.cs
+++ lab2 TCPP/Program.cs
@@ -6,7 +6,7 @@ namespace lab2_TCPP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from browser!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello1 World!");
